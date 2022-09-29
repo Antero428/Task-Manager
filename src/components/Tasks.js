3dd24a21aaa7430972c88f9d041ed8b4408226de
DylanWalsh <dawals22@colby.edu>
2022-09-29 10:11:22 -0400

    initial commit

---
+++ src/components/Tasks.js
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id:1,
+            text: 'Comp Sci Project',
+            day: 'September 15th at 11:59pm',
+            reminder: true,
+        },
+        {
+            id:2,
+            text: 'Counseling Appt',
+            day: 'September 20th at 11:00am',
+            reminder: true,
+        },
+        {
+            id:3,
+            text: 'Presentation',
+            day: 'September 25th at 10:00am',
+            reminder: true,
+        },
+    ])
+
+    return (
+        <>
+        {tasks.map((task) => (
+            <h3 key={task.id}>{task.text}</h3>
+        ))}
+        </>
+    )
+}
+
+export default Tasks
